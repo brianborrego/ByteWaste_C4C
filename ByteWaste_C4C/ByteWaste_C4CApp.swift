5ce3bed58f260ee7b4c643d899c99f2e8cced4b7
Floormatt89 <86789069+FloorMatt89@users.noreply.github.com>
2026-02-07 13:13:49 -0500

    First Commit Baby

---
+++ ByteWaste_C4C/ByteWaste_C4CApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByteWaste_C4CApp.swift
+//  ByteWaste_C4C
+//
+//  Created by Matthew Segura on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ByteWaste_C4CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByteWaste_C4C/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ByteWaste_C4C
+//
+//  Created by Matthew Segura on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
